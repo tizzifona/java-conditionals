61c8fe2368ac303763641e5d308a9c137fd71f62
tizzifona <tizzifona@gmail.com>
2025-05-06 10:48:03 +0200

    Added code for exercise Traffic Lights

--- TrafficLights.java
+++ TrafficLights.java
@@ -1,13 +1,31 @@
 public class TrafficLights {
+
+    enum Color {
+        AMARILLO,
+        VERDE,
+        ROJO
+    }
     public static void main(String[] args) {
 
         //Escribe un programa que basado en el color del semáforo verde, amarillo o rojo determine las acciones de un conductor 
-        
+        Color colorSemáforo = Color.AMARILLO;
         //Si es verde imprime en la terminal: Puedes seguir
+        if (colorSemáforo == Color.VERDE){
+            System.out.println("Puedes seguir");
+        }
         //Si es amarillo imprime en la terminal: Ten cuidado
+        else if (colorSemáforo == Color.AMARILLO) {
+            System.out.println("Ten cuidado");
+        }
         //Si es rojo imprime en la terminal: ¡¡¡Para!!!
-        //Si es otro color imprime en la terminal: El semáforo está dañado
 
+        else if (colorSemáforo == Color.ROJO){
+            System.out.println("¡¡¡Para!!!");
+        }
+        //Si es otro color imprime en la terminal: El semáforo está dañado
+        else {
+            System.out.println("El semáforo está dañado");
+        }
         //Cambia el valor de la variable, corre el programa y mira el resultado 
 
     }
